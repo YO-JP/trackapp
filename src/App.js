867a6c01ac2e19b4a5f7bb145e50364b2354a015
Yuta Obara <o.yuta722@gmail.com>
2021-02-10 14:56:09 +0900

    react-router structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navbar from "./components/navbar.js";
+import ExerciseList from "./components/exercise-list";
+import ExerciseEdit from "./components/exercise-edit";
+import ExerciseCreate from "./components/exercise-create";
+import UserCreate from "./components/user-create";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" component={ExerciseList} />
+      <Route path="/edit/:id" component={ExerciseEdit} />
+      <Route path="/create" component={ExerciseCreate} />
+      <Route path="/user" component={UserCreate} />
+    </Router>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render () {
+        return(
+            <nav className="">
+                <Link to="/" className="">Exercise Tracker</Link>
+                <div>
+                    <ul>
+                        <li>
+
+                        </li>
+                        <li>
+
+                        </li>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+    }
+}
