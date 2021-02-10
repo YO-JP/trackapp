5d17a248b375bcdd5c9a3989221dc68674964c52
Yuta Obara <o.yuta722@gmail.com>
2021-02-10 16:41:28 +0900

    all functionality, minimum styling

--- src/components/exercise-create.js
+++ src/components/exercise-create.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios'
 
 export class ExerciseCreate extends Component {
     constructor(props) {
@@ -22,9 +23,14 @@ export class ExerciseCreate extends Component {
     }
 
     componentDidMount () {
-        this.setState({
-            users: ['test user'],
-            username: 'tes user'
+        axios.get('http://localhost:5000/user/')
+        .then(response => {
+            if (response.data.length > 0) {
+                this.setState({
+                    users: response.data.map(user => user.username),
+                    username: response.data[0].username
+                })
+            }
         })
     }
     
@@ -62,6 +68,9 @@ export class ExerciseCreate extends Component {
         }
         console.log(exercise)
 
+        axios.post('http://localhost:5000/exercise/add', exercise)
+            .then(res => console.log(res.data))
+
         window.location = "/"
     }
 
@@ -97,7 +106,7 @@ export class ExerciseCreate extends Component {
                     />
               </div>
               <div className="form-group">
-                <label>エクササイズ時間 (in minutes): </label>
+                <label>エクササイズ時間 (分): </label>
                 <input 
                     type="text" 
                     className="form-control"
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,17 +5,17 @@ export default class Navbar extends Component {
     render () {
         return(
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">エクササイズ・メモ</Link>
-                <div className="collapse navbar-collapse">
+                <Link to="/" className="navbar-brand">ExerciseAPP</Link>
+                <div className=" navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                            <Link to ="/" className="nav-link">エクササイズ</Link>
+                            <Link to ="/" className="nav-link">エクササイズ・リスト</Link>
                         </li>
                         <li className="navbar-item">
-                            <Link to ="/create" className="nav-link">エクササイズ　ログ入力</Link>
+                            <Link to ="/create" className="nav-link">ログ入力</Link>
                         </li>
                         <li className="navbar-item">
-                            <Link to ="/user" className="nav-link">新ユーザー</Link>
+                            <Link to ="/user" className="nav-link">新ユーザー登録</Link>
                         </li>
                     </ul>
                 </div>
